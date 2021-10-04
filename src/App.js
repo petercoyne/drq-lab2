8d415e0a369857f3f3df6e81e54c10a60fa61144
Peter Coyne <g00266507@gmit.ie>
2021-10-04 12:28:23 +0100

    Completed lab

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
 import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Content/>
-        <Footer/>
+        <Router>
+          <Switch>
+            <Route path='/' component={Content} exact/>
+            <Route path='/read' component={Header} exact/>
+            <Route path='/create' component={Footer} exact/>
+          </Switch>
+        </Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
